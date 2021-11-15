f0d9f61950bb4da099cfafc1fa9c5e3d06b2db63
Lê Trung Nghĩa <90697464+KyoNo2k1@users.noreply.github.com>
2021-11-15 20:31:52 +0700

    push Link

--- src/Pages/HomePage/GameCell/game-cell.jsx
+++ src/Pages/HomePage/GameCell/game-cell.jsx
@@ -2,18 +2,19 @@ import React from "react";
 import axios from "axios";
 import "./game-cell.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from "react-router-dom";
 
 function GameCell({game}){
     const gameAvaUrl = axios.defaults.baseURL + 'uploads/images/games/avatar/' + game.Avatar;
 
     return (
         <div className="game-cell all-games" data-item={game.Category}>
-            <a className="game-cell-link" href={'/game-detail/' + game.id + "/" + game.Url}>
+            <Link className="game-cell-link" to={'/game-detail/' + game.id + "/" + game.Url}>
                 <img className="game-cell__img" src={gameAvaUrl} alt="Cannot get" />
                 <div className="game-cell__name">{game.Title}</div>
                 <div className="game-cell__rate">Rate: {game.Rate} / 10</div>
                 <div className="game-cell__played">Played: {game.Played}</div>
-            </a>
+            </Link>
         </div>
     );
 }
