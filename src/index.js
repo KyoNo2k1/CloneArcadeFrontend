936428667cee1fdb83e7f70b295589248f9d6b4e
Lê Trung Nghĩa <90697464+KyoNo2k1@users.noreply.github.com>
2021-11-15 12:19:05 +0700

    a

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import 'bootstrap/dist/css/bootstrap.css'
 import axios from 'axios'
 
 // axios.defaults.baseURL = 'http://localhost:8000/';
-axios.defaults.baseURL = 'https://demoarcadegame.herokuapp.com/'
+// axios.defaults.baseURL = 'https://demoarcadegame.herokuapp.com/'
+axios.defaults.baseURL = 'https://arcadegame-gonin.herokuapp.com/'
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
